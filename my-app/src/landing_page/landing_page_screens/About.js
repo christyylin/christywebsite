87a7a556c392d120d9e0be52279f378479738982
Natan Vidra <vidranatan@gmail.com>
2024-09-12 20:04:12 -0400

    updated

--- my-app/src/landing_page/landing_page_screens/About.js
+++ my-app/src/landing_page/landing_page_screens/About.js
@@ -13,21 +13,21 @@ function About() {
                 style={{ width: '55%', height: 'auto', marginLeft: '12px' }}
               />
 
-              <div className="ml-3">
-                <h3 className="text-4xl font-medium text-left text-[#213A52] ml-[-150px] pl-20">about me</h3>
-                <div className="container ml-[-150px] w-15">
-                    <p className ="text-xl font-medium text-left text-black mr-40 pl-20 pt-5">
+              <div className="ml-3 mr-10">
+                <h3 className="text-4xl font-medium text-left text-[#213A52]">about me</h3>
+                {/* <div className="container"> */}
+                    <p className ="text-xl font-medium text-left text-black pt-5">
                     Hi there! I’m Christy and I recently graduated from UC Berkeley Haas School of Business.
-                    </p>
-                    <p className ="text-xl font-medium text-left text-black mr-40 pl-20 pt-5">
+                    <br></br>
+                    <br></br>
                     I’m currently a product manager and product designer based in New York City & have done projects for companies such as: Mastercard, Snapchat, Miro, Tower28, and a variety of early-stage startups. I love working with engineers & designers to turn revolutionary ideas into impactful products.
                     </p>
 
-                </div>
+                {/* </div> */}
               </div>
-            </div> 
-             
-            
+            </div>
+
+
         </div>
     );
 }
